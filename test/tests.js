91f442a99ad81ae9afe745e4d8970e553edcdd2d
Andrei Mircescu <mircescu.andrei@gmail.com>
2016-06-01 01:29:01 +0300

    Add unit tests, neighbors api does not work properly

---
+++ test/tests.js
@@ -0,0 +1,100 @@
+var chai = require("chai");
+var expect = chai.expect;
+
+var graphthis = require("../lib/graphthis.js");
+
+describe("graphthis", function() {
+	it("Checks if it can create and add 2 nodes to the graph", function() {
+		var graph = new graphthis.graph("Test Graph");
+		var nodeA = new graphthis.node("nodeA",5,null);
+		var nodeB = new graphthis.node("nodeB",10,null);
+		graph.addNode(nodeA);
+		graph.addNode(nodeB);
+		expect(graph.nodeList.length).to.equal(2);
+	});
+});
+
+describe("graphthis", function() {
+	it("Checks if we can create an edge between 2 nodes", function() {
+		var graph = new graphthis.graph("Test Graph");
+		var nodeA = new graphthis.node("nodeA",5,null);
+		var nodeB = new graphthis.node("nodeB",10,null);
+		graph.createEdge(nodeA,nodeB,"EdgeLabel", 10);
+		//check if nodeA is originate
+		expect(nodeA.asOriginateEndpointFor[0].node).to.equal(nodeB);
+		//check if nodeB is destination
+		expect(nodeB.asDestinationEndpointFor[0].node).to.equal(nodeA);
+	});
+});
+
+describe("graphthis", function() {
+	it("Checks if addNodes work", function() {
+		var graph = new graphthis.graph("Test Graph");
+		var nodeA = new graphthis.node("nodeA",5,null);
+		var nodeB = new graphthis.node("nodeB",10,null);
+		var nodeC = new graphthis.node("nodeC",15,null);
+		graph.addNodes(nodeA,nodeB,nodeC);
+		expect(graph.nodeList.length).to.equal(3);
+
+	});
+});
+
+describe("graphthis", function() {
+	it("Checks if addGroup works", function() {
+		var graph = new graphthis.graph("Test Graph");
+		var nodeA = new graphthis.node("nodeA",5,null);
+		var nodeB = new graphthis.node("nodeB",10,null);
+		var nodeC = new graphthis.node("nodeC",15,null);
+		nodeA.addGroup("group1");
+		nodeB.addGroup("group1");
+		nodeC.addGroup("group2");
+		
+		graph.addNodes(nodeA,nodeB,nodeC);
+		expect(graph.getNodesByGroup("group1").length).to.equal(2);
+		expect(graph.getNodesByGroup("group2").length).to.equal(1);
+		
+	});
+});
+
+describe("graphthis", function() {
+	it("Checks if adjacent works", function() {
+		var graph = new graphthis.graph("Test Graph");
+		var nodeA = new graphthis.node("nodeA",5,null);
+		var nodeB = new graphthis.node("nodeB",10,null);
+		var nodeC = new graphthis.node("nodeC",15,null);
+		nodeA.addGroup("group1");
+		nodeB.addGroup("group1");
+		nodeC.addGroup("group2");
+		graph.addNodes(nodeA,nodeB,nodeC);
+
+		graph.createEdge(nodeA,nodeB,"EdgeLabel", 10);
+		graph.createEdge(nodeB,nodeC,"EdgeLabel2", 20);
+		expect(graph.adjacent(nodeA,nodeB)).to.equal(true);
+		expect(graph.adjacent(nodeB,nodeC)).to.equal(true);
+		expect(graph.adjacent(nodeA,nodeC)).to.equal(false);
+
+	});
+});
+
+/*describe("graphthis", function() {
+	it("Checks if neighbors works", function() {
+		var graph = new graphthis.graph("Test Graph");
+		var nodeA = new graphthis.node("nodeA",5,null);
+		var nodeB = new graphthis.node("nodeB",10,null);
+		var nodeC = new graphthis.node("nodeC",15,null);
+		nodeA.addGroup("group1");
+		nodeB.addGroup("group1");
+		nodeC.addGroup("group2");
+		graph.addNodes(nodeA,nodeB,nodeC);
+
+		graph.createEdge(nodeA,nodeB,"EdgeLabel", 10);
+		graph.createEdge(nodeB,nodeC,"EdgeLabel2", 20);
+		graph.createEdge(nodeB,nodeA,"EdgeLabel3", 30);
+		
+		expect(graph.neighbors(nodeA).length).to.equal(1);
+		expect(graph.neighbors(nodeB).length).to.equal(2);
+
+
+
+	});
+});*/
